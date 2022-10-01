a9b6e3e1fb0368fd4ce775adb7031bc37e9cfa01
newt-rgb <1533245086@qq.com>
2022-10-01 13:54:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from calculator_ui import Ui_MainWindow
+from calculator_cal import mywindow
+
+#实例化mywindow对象，展示页面
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    w = mywindow()
+    w.show()
+    sys.exit(app.exec_())
+
