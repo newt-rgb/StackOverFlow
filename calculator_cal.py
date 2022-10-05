43bfdc0bec103e79bf655443f8a1f0d1e95504d1
Sh1nyruo <17701702984@163.com>
2022-10-05 23:43:49 +0800

    完成了添加待办事项数据的结构与存储

--- calculator_cal.py
+++ calculator_cal.py
@@ -1,4 +1,4 @@
-#主要完成在计算器中点击事件的信号和事件函数的连接
+# 主要完成在计算器中点击事件的信号和事件函数的连接
 
 
 import sys
@@ -6,18 +6,20 @@ import math
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
 from calculator_ui import Ui_MainWindow
 from CustomDialog import CustomDialog
-#创建mywindow类，继承于UI设计中的UI_MainWindow类
+from math import *
+from todoWindow import todoWindow
+
+# 创建mywindow类，继承于UI设计中的UI_MainWindow类
 
 class mywindow(QMainWindow, Ui_MainWindow):
 
-    #构造函数
+    # 构造函数
     def __init__(self):
-        #实现父类函数的构造
+        # 实现父类函数的构造
         super().__init__()
-        self.setupUi(self)
-        
-        #以下为对UI中的按钮的同事件函数的连接
-        #回退按钮
+        self.setupUi(self)        
+        # 以下为对UI中的按钮的同事件函数的连接
+        # 回退按钮
         self.delButton.clicked.connect(self.backspace)
         # 1-0按钮
         self.oneButton.clicked.connect(self.one)
@@ -37,160 +39,202 @@ class mywindow(QMainWindow, Ui_MainWindow):
         self.divdButton.clicked.connect(self.divide)
         # 点
         self.dotButton.clicked.connect(self.dot)
-        #取余
+        # 取余
         self.percentButton.clicked.connect(self.percentage)
-        #等于
+        # 等于
         self.equalButton.clicked.connect(self.calculate)
         # 清空缓存
         self.cButton.clicked.connect(self.clear_all)
-        #三角函数
+        # 三角函数
         self.sinButton.clicked.connect(self.Sin)
         self.cosButton.clicked.connect(self.Cos)
         self.tanButton.clicked.connect(self.Tan)
         self.piButton.clicked.connect(self.Pi)
-        #左右括号
+        # 左右括号
         self.leftButton.clicked.connect(self.leftBkt)
         self.rightButton.clicked.connect(self.rightBkt)
-        #阶乘、平方、立方、n次方
+        # 阶乘、平方、立方、n次方
         self.factButton.clicked.connect(self.Factorial)
         self.sqrtButton.clicked.connect(self.Sqrt)
         self.squareButton.clicked.connect(self.Square)
         self.cubeButton.clicked.connect(self.Cube)
+        # 添加日程按键
+        self.addw = None
+        self.addtodo.clicked.connect(self.addToDoList)
 
-    #以下为事件函数具体实现部分
-    #text用来记录计算机真正计算的表达式，利用eval(self.text)函数完成实现
-    text =""
-
-    #回退函数
+    # 以下为事件函数具体实现部分
+    result = 0
+    #回退按钮
     def backspace(self):
         self.lineEdit.backspace()
-        self.text = self.text[:-1]
-    
-    #按键0-9
+    #数字1-9
     def one(self):
         self.lineEdit.insert('1')
-        self.text += "1"
-        
+
     def two(self):
         self.lineEdit.insert('2')
-        self.text += "2"
+
     def three(self):
         self.lineEdit.insert('3')
-        self.text += "3"
+
     def four(self):
         self.lineEdit.insert('4')
-        self.text += "4"
+
     def five(self):
         self.lineEdit.insert('5')
-        self.text += "5"
+
     def six(self):
         self.lineEdit.insert('6')
-        self.text += "6"
+
     def seven(self):
         self.lineEdit.insert('7')
-        self.text += "7"
+
     def eight(self):
-        self.text += "8"
         self.lineEdit.insert('8')
+
     def nine(self):
-        self.text += "9"
         self.lineEdit.insert('9')
+
     def zero(self):
-        self.text += "0"
         self.lineEdit.insert('0')
 
-    #运算符按键
+    #加减乘除
     def plus(self):
-        self.text += "+"
         self.lineEdit.insert('+')
-    def minus(self):
-        self.text += "-"
-        self.lineEdit.insert('-')
-    def multiply(self):
-        self.text += "*"
-        self.lineEdit.insert('*')
-    def divide(self):
-        self.text += "/"
-        self.lineEdit.insert('/')
 
-    #点按键
+    def minus(self):
+        self.lineEdit.insert('-')
+
+    def multiply(self):
+        self.lineEdit.insert('*')
+
+    def divide(self):
+        self.lineEdit.insert('/')
+    #小数点
     def dot(self):
-        self.text += "."
         self.lineEdit.insert('.')
-    #百分号按键
+
+    # 百分号按键
     def percentage(self):
         self.lineEdit.insert('%')
-        self.text += "* 0.01"
 
-    #清除按键
+    # 清除按键
     def lineEdit_clear(self):
-        self.text =""
         self.lineEdit.clear()
+
     def clear_all(self):
-        self.text = ""
         self.lineEdit.clear()
-    #等号按键
-    
+
+    #三角函数按键
+    def Sin(self):
+        self.lineEdit.insert("sin(")
+
+    def Cos(self):
+        self.lineEdit.insert("cos(")
+
+    def Tan(self):
+        self.lineEdit.insert("tan(")
+
+    def Pi(self):
+        self.lineEdit.insert("π")
+
+    #左右括号按键
+    def leftBkt(self):
+        self.lineEdit.insert('(')
+
+    def rightBkt(self):
+        self.lineEdit.insert(')')
+
+    #阶乘、平方、立方、开方按键
+    def Factorial(self):
+        self.lineEdit.insert("!")
+
+    def Sqrt(self):
+        self.lineEdit.insert("√(")
+
+    def Square(self):
+        self.lineEdit.insert(")^2")
+
+    def Cube(self):
+        self.lineEdit.insert(")^3")
+
+    #搜寻对应左括号并返回其位置
+    def position(self, n):
+        texttmp = self.lineEdit.text()
+        postmp = 0
+        for i in range(n - 1, -1, -1):
+            if (texttmp[i] == ")"):
+                postmp += 1
+            if (texttmp[i] == "("):
+                postmp -= 1
+            if (postmp == 0):
+                return i
+
+    #对运算结果进行优化
+    def trans(self):
+        res = str(self.result)
+        for i in range(len(res)):
+            if (res[i] == '.'):
+                if (len(res) - i >= 6):
+                    self.result = round(self.result, 2)
+                self.result = round(self.result,10)
+                res = str(self.result)
+                if(i == len(res)-2):
+                    if(res[-1] == '0'):
+                       self.result= round(self.result)
+                break
+
+    #每次根据显示板上的公式进行计算
     def calculate(self):
         try:
+            text = self.lineEdit.text()
+            # 计算判断
+            for n in range(len(text)):
+
+                # 解决阶乘问题
+                if (text[n] == "!"):
+                    if (text[n - 1] != ')'):
+                        for i in range(n):
+                            m = n - i - 1
+                            if (m > 0 and text[m].isdigit()):
+                                continue
+                            else:
+                                if (m != 0):
+                                    text = text[:n - i] + "factorial(" + text[n - i:n] + ")" + text[n + 1:]
+                                    break
+                                else:
+                                    text = "factorial(" + text[:n] + ")" + text[n + 1:]
+                                    break
+                    else:
+                        # 判断当需要计算括号里面的阶乘值
+                        pos = self.position(n)
+                        text = text[:pos] + "factorial" + text[pos:n] + text[n + 1:]
+                # 计算次方
+                if (text[n] == "^"):
+                    text = text[:n] + "**" + text[n + 1:]
+                # 计算π值
+                if (text[n] == "π"):
+                    text = text[:n] + "math.pi" + text[n + 1:]
+                # 计算%
+                if (text[n] == "%"):
+                    text = text[:n] + "*0.01" + text[n + 1:]
+                # 计算开方值
+                if (text[n] == "√"):
+                    text = text[:n] + "math.sqrt" + text[n + 1:]
+            # 计算判断结束
             self.lineEdit.clear()
-            result = eval(self.text)
-            self.lineEdit.insert(str(result))
-            self.text = str(result)
+            self.result = eval(text)
+            # 优化结果
+            self.trans()
+            self.lineEdit.insert(str(self.result))
         except:
             dlg = CustomDialog()
             dlg.exec_()
             self.text = ""
             self.lineEdit.clear()
-        
-    #三角函数按键
-    def Sin(self):
-        self.text += "math.sin("
-        self.lineEdit.insert("sin(")
-    def Cos(self):
-        self.text += "math.cos("
-        self.lineEdit.insert("cos(")
-    def Tan(self):
-        self.text += "math.tan("
-        self.lineEdit.insert("tan(")
-    def Pi(self):
-        self.text += "math.pi"
-        self.lineEdit.insert("π")
+    
+    # 添加日程
+    def addToDoList(self, checked):
+        self.addw = todoWindow(self)
+        self.addw.show()
 
-    #左右括号按键
-    def leftBkt(self):
-        self.text += "("
-        self.lineEdit.insert('(')
-    def rightBkt(self):
-        self.text += ")"
-        self.lineEdit.insert(')')
-
-    #次方按键
-    def Factorial(self):
-        self.lineEdit.insert("!")
-        for i in range(len(self.text)):
-            if(self.text[len(self.text)-1-i:][0].isdigit() & len(self.text)-1-i!=0):
-                continue
-            if (i == len(self.text)-1):
-                self.text = "math.factorial(" + self.text + ")"
-                break
-            else:
-                text_tmp = "math.factorial("+self.text[0-i:]+")"
-                self.text = self.text[:0-i]
-                self.text += text_tmp
-                break
-    
-    def Sqrt(self):
-        self.text += "math.sqrt("
-        self.lineEdit.insert("√(")
-    
-    
-    def Square(self):
-        # self.Power()
-        self.lineEdit.insert(")^2")
-        self.text += ") ** 2"
-        
-    def Cube(self):
-        # self.Power()
-        self.lineEdit.insert(")^3")
-        self.text += ") ** 3"
---
+++ todoWindow.py
@@ -0,0 +1,118 @@
+
+import sys
+from PyQt5.QtCore import Qt
+import json
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
+from CustomDialog import CustomDialog
+from calendar_ui import Ui_addmenu
+from PyQt5.QtCore import QDateTime,QDate,QTime
+from datetime import date,time,datetime,timedelta
+
+class todoWindow(QWidget, Ui_addmenu):
+    def __init__(self, parent, curtime = datetime.today()):
+        #设置ui
+        super().__init__()
+        self.setupUi(self)
+        
+        #设置存储结构
+        self.todolist = []
+        self.load()
+        #设置父窗口，父窗口关闭，子窗口同时关闭
+        self.setParent(parent)
+        self.setWindowFlags(Qt.Window)
+        
+        #设置待办事项：内容
+        self.content = ""
+        self.todoText.textChanged.connect(self.todoTextEdit)
+        
+        #设置待办事项：截止时间
+        self.ddl = curtime
+        self.ddlText.setDateTime(curtime)
+        self.ddlText.setCurrentSectionIndex(2)
+        self.ddlText.setMinimumDateTime(datetime.today())
+        self.ddlText.dateTimeChanged.connect(self.ddlTextEdit)
+        
+        #设置待办事项：是否提醒
+        self.alarmflag = False
+        self.alarm.stateChanged.connect(self.alarmEdit)
+        
+        #设置提醒周期：提前m天，每隔n小时一次
+        self.advanceDay = 0
+        self.altHour = 24
+        self.advDay.setMaximum(0)
+        self.altDay.setValue(24)
+        self.advDay.textChanged.connect(self.advDayEdit)
+        self.altDay.textChanged.connect(self.altDayEdit)
+        
+        #计算截止时间
+        self.delta_1 = timedelta(days = 0,hours = 0)
+        self.delta_2 = timedelta(days = 0,hours = 0)
+        self.nextalarm = self.ddl
+        self.deltaDay_1.setText("0")
+        self.deltaHour_1.setText("0")
+        self.deltaDay_2.setText("0")
+        self.deltaHour_2.setText("0")
+        
+        #确认添加日程
+        self.confirmButton.clicked.connect(self.confirm)
+        
+    #待办事项内容管理
+    def todoTextEdit(self):
+        self.content = self.todoText.toPlainText()
+        self.content = self.content.strip()
+
+    #待办事项截止时间处理
+    def ddlTextEdit(self):
+        self.ddl = self.ddlText.dateTime().toPyDateTime() 
+        self.delta_1 = self.ddl - datetime.today()
+        d_d = str(self.delta_1.days)
+        d_h = str(int(self.delta_1.seconds / 3600))
+        self.deltaDay_1.setText(d_d)
+        self.deltaHour_1.setText(d_h)
+        
+        self.nextalarm = self.ddl - timedelta(days = self.advanceDay)
+        self.delta_2 = self.nextalarm - datetime.today()
+        d2_d = str(self.delta_2.days)
+        d2_h = str(int(self.delta_2.seconds / 3600))
+        self.deltaDay_2.setText(d2_d)
+        self.deltaHour_2.setText(d2_h)
+        #处理提前几天信息，不能超过当前时间
+        self.advDay.setMaximum(self.delta_1.days if self.delta_1.days >= 0 else 0)
+        
+    #待办事项提醒处理
+    def alarmEdit(self):
+        self.alarmflag = self.alarm.isChecked()
+
+    #处理提前多少天
+    def advDayEdit(self):
+        self.advanceDay = self.advDay.value()
+        adv_d = timedelta(days = self.advanceDay)
+        self.nextalarm = self.ddl - adv_d
+        self.delta_2 = self.nextalarm - datetime.today()
+        d2_d = str(self.delta_2.days)
+        d2_h = str(int(self.delta_2.seconds / 3600))
+        self.deltaDay_2.setText(d2_d)
+        self.deltaHour_2.setText(d2_h)
+    
+    #处理每隔多少小时
+    def altDayEdit(self):
+        self.altHour = self.altDay.value()
+
+    def confirm(self):
+        if self.content:
+            todos = [self.content,datetime.strftime(self.ddl,"%Y-%m-%d %H:%M"),self.alarmflag,self.advanceDay,self.altHour]
+            self.todolist.append(todos)
+            with open("data.json", "w") as f:
+                json.dump(self.todolist, f)
+            self.close()
+        else:
+            dlg = CustomDialog("待办事项内容不能为空！请重新输入。")
+            dlg.exec_()
+        
+
+    def load(self):
+        try:
+            with open("data.json","r") as f:
+                self.todolist = json.load(f)
+        except Exception:
+            pass
